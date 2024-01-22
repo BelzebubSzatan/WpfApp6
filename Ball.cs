b19e2f7833a146c09f956de7c5d6c36320e01815
KacperMatlag <matlagkacper@gmail.com>
2024-01-22 19:14:01 +0100

    Add Ball properties 😅

---
+++ Ball.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApp6 {
+    public class Ball : ICanvasObject {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Canvas Canvas { get; set; }
+        public double Angle { get; set; }
+        public int DirectionX { get; set; }
+        public int DirectionY { get; set; }
+        public Ellipse Shape { get; set; }
+        public double Speed { get; set; }
+
+        public Ball(int width, int height, Canvas canvas) {
+            Width = width;
+            Height = height;
+            Canvas = canvas;
+            Shape = new Ellipse() {
+                Width = width,
+                Height = height,
+                Fill = new SolidColorBrush(Colors.White),
+            };
+            DirectionX = 1;
+            DirectionY = 1;
+            Random r = new();
+            Angle = r.Next(30, 60) * Math.PI / 180;
+            Speed = 3;
+            Canvas.Children.Add(Shape);
+        }
+
+        public void Draw() {
+            throw new NotImplementedException();
+        }
+
+        public void Reset() {
+            throw new NotImplementedException();
+        }
+    }
+}
