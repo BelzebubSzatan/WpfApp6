d6899729d2ebe7c6e995e96b6f5b30fc9d7ce7b7
KacperMatlag <matlagkacper@gmail.com>
2024-01-22 19:09:40 +0100

    Add timer to MainWindow 🕛

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,14 +8,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp6 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        DispatcherTimer timer;
         public MainWindow() {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(16);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e) {
+            throw new NotImplementedException();
         }
 
         private void Window_MouseMove(object sender, MouseEventArgs e) {
